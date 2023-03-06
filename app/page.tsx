c585153ed34110c319c63d97ddb73b75fb42072a
EmmGlezz <contact@emilianogonzalez.com>
2023-03-06 20:00:09 +1100

    Metadata modified

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,14 @@ import HeroSection from "@/components/HeroSection"
 import AboutSection from "@/components/AboutSection"
 import ProjectsSection from "@/components/ProjectsSection"
 
+export const metadata = {
+    title: 'Emiliano González',
+    description: 'Emiliano González personal portfolio',
+    icons: {
+      icon: '/code.png',
+    },
+  }
+
 export default function Home() {
   return (
    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
